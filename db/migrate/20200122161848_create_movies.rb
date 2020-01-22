55e7173535b8a067dcc4b0a2f510fd0eb9e38ec2
Sherilyn Moline <smoline@gmail.com>
2020-01-22 11:32:12 -0500

    add movie model

---
+++ db/migrate/20200122161848_create_movies.rb
@@ -0,0 +1,16 @@
+class CreateMovies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :description
+      t.integer :tmdb_id
+      t.string :imdb_id
+      t.string :realease_date
+      t.integer :runtime
+      t.string :tagline
+      t.string :movie_image_url
+
+      t.timestamps
+    end
+  end
+end
