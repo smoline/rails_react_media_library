c8c4a2750b6a6aa6a6d07208bf18d8c283bcf39a
Sherilyn Moline <smoline@gmail.com>
2020-01-27 14:18:46 -0500

    added logout to navbar

--- app/javascript/components/navigation.jsx
+++ app/javascript/components/navigation.jsx
@@ -1,31 +1,52 @@
-import React, { Component } from 'react'
-import { Menu } from 'semantic-ui-react'
+import React, { Component } from "react"
+import { Menu, Input } from "semantic-ui-react"
 
 export default class Navigation extends Component {
-  state = { activeItem: 'Movies' }
+  constructor(props) {
+    super(props)
+    this.state = {
+      activeItem: "Movies"
+    }
+  }
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  handleItemClick = (e, { name }) => {
+    this.setState({ activeItem: name })
+    if (name === "Logout") {
+      this.props.handleLogout()
+    }
+  }
 
   render() {
+    console.log(this.props)
     const { activeItem } = this.state
 
     return (
       <Menu inverted>
         <Menu.Item
-          name='Movies'
-          active={activeItem === 'Movies'}
+          name="Movies"
+          active={activeItem === "Movies"}
           onClick={this.handleItemClick}
         />
         <Menu.Item
-          name='TV Shows'
-          active={activeItem === 'TV Shows'}
+          name="TV Shows"
+          active={activeItem === "TV Shows"}
           onClick={this.handleItemClick}
         />
         <Menu.Item
-          name='Games'
-          active={activeItem === 'Games'}
+          name="Games"
+          active={activeItem === "Games"}
           onClick={this.handleItemClick}
         />
+        <Menu.Menu position="right">
+          <Menu.Item>
+            <Input icon="search" placeholder="Search..." />
+          </Menu.Item>
+          <Menu.Item
+            name="Logout"
+            active={activeItem === "Logout"}
+            onClick={this.handleItemClick}
+          />
+        </Menu.Menu>
       </Menu>
     )
   }
