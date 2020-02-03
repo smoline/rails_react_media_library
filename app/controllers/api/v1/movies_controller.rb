0e88d397c36daec9cfee92877a57892b81854975
Sherilyn Moline <smoline@gmail.com>
2020-02-03 10:30:58 -0500

    TMDB movie search added to movie controller

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -10,12 +10,13 @@ class Api::V1::MoviesController < ApplicationController
   end
 
   def create
-    movie = Movie.create!(movie_params)
-    if movie
-      render json: movie
-    else
-      render json: movie.errors
-    end
+    puts params
+    # movie = Movie.create!(movie_params)
+    # if movie
+    #   render json: movie
+    # else
+    #   render json: movie.errors
+    # end
   end
 
   def show
@@ -37,11 +38,33 @@ class Api::V1::MoviesController < ApplicationController
     # TODO: once created, destroy the owner record only
   end
 
+  def movies_search
+    title = params[:title]
+    movie = Movie.find_or_initialize_by(title: params[:title])
+    if movie.new_record?
+      movie_info = MovieSearch.find_initial_movie_info(title)
+      render json: movie_info
+    else
+      render json: movie
+    end
+  end
+
+  def movie_info
+    tmdb_id = params[:tmdb_id]
+    more_movie_info = MovieSearch.find_more_movie_info(tmdb_id)
+    movie = Movie.find_or_initialize_by(tmdb_id: params[:tmdb_id])
+    if movie.new_record?
+      render json: more_movie_info
+    else
+      render json: movie
+    end
+  end
+
   private
 
   def movie_params
     # params.permit(:title, :description, :tmdb_id, :imdb_id, :release_date, :runtime, :tagline, :movie_image_url)
-    params.require(:movie).permit(:title, :tmdb_id, :description, :release_date, :runtime, :tagline, :movie_image_url, owners_attributes: [:id, :user_id, :upc, :notes, :rating, :ownable_type, :ownable_id])
+    params.require(:movie).permit(:title, :tmdb_id, :description, :release_date, :runtime, :tagline, :movie_image_url, owners_attributes: [:user_id, :upc, :notes, :rating, :ownable_type, :ownable_id])
   end
 
   def movie
