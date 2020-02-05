b2fc6b96b9ff5154b296666340c71ddb97f39ab8
Sherilyn Moline <smoline@gmail.com>
2020-02-05 14:22:24 -0500

    movie associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,10 +3,7 @@ class Movie < ApplicationRecord
   include Castable
   include Crewable
 
-  has_many :owners, as: :ownable
   accepts_nested_attributes_for :owners
-
-  has_many :users, through: :owners
 
   validates :title, presence: true
   validates_uniqueness_of :tmdb_id
