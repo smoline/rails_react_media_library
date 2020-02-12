3828aab360970dcac484d231d0b742b53196d4c4
Sherilyn Moline <smoline@gmail.com>
2020-02-12 11:28:21 -0500

    movies index test

---
+++ spec/javascript/movies-index/movies.test.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Movies from '../../../app/javascript/components/movies-index/movies'
+
+const history = {
+  push: jest.fn()
+}
+
+describe('Movies', () => {
+  describe('componentDidMount', () => {
+    it('sets the state componentDidMount', async () => {
+
+      window.fetch = jest.fn().mockImplementation(() => {
+        return new Promise((resolve, reject) => {
+          resolve({
+            status: 200,
+            ok: true,
+            json: () => new Promise((resolve, reject) => {
+              resolve({
+                  movies: [
+                    {
+                      id: 25,
+                      title: "Amadeus",
+                      description: "The incredible story of genius musician Wolfgang Amadeus Mozart, told in flashback by his peer and secret rival Antonio Salieri – now confined to an insane asylum.",
+                      tmdb_id: 279,
+                      imdb_id: "",
+                      release_date: "1984-10-26",
+                      runtime: 160,
+                      tagline: "...Everything you've heard is true",
+                      movie_image_url: "http://image.tmdb.org/t/p/w185//flnoqdC38mbaulAeptjynOFO7yi.jpg",
+                      created_at: "2020-01-28T15:52:51.042Z",
+                      updated_at: "2020-01-28T15:52:51.042Z",
+                      owner: {
+                        id: 25,
+                        notes: "",
+                        upc: null,
+                        rating: 3,
+                        user_id: 1,
+                        ownable_type: "Movie",
+                        ownable_id: 25,
+                        created_at: "2020-01-28T15:52:51.046Z",
+                        updated_at: "2020-01-28T15:52:51.046Z"
+                      }
+                    }
+                  ]
+              })
+            })
+          })
+        })
+      })
+
+      const component = shallow(
+        <Movies
+          history={history}
+          movies={[]}
+        />
+      )
+      
+      expect(component.exists()).toBe(true)
+      expect(component.state('movies')).toBeTruthy()
+      setImmediate(() => {
+        console.log(component.state())
+        expect(component.state().movies[0].id).toEqual(25)
+      })
+    })
+  })
+})
