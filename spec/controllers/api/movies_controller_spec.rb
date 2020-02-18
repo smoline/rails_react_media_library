1ab3271a55a701478273db6383ba8b017010796d
Sherilyn Moline <smoline@gmail.com>
2020-02-18 14:49:05 -0500

    movie controller test added

---
+++ spec/controllers/api/movies_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::MoviesController, type: :controller do
+  describe "GET movies#index" do
+    it "should list all movies belonging to a user" do
+      skip
+    end
+  end
+
+  describe "GET movies#show" do
+    it "should get a specific movie as json" do
+      skip
+    end
+  end
+end
