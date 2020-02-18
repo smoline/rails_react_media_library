2fa17b6be96204bf4cb84d1adb745264f9e38b9d
Sherilyn Moline <smoline@gmail.com>
2020-02-18 14:49:47 -0500

    user controller test

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  describe "GET users#index" do
+    it "should list all users" do
+      skip
+    end
+  end
+
+  describe "GET users#show" do
+    it "should get a specific user as json" do
+      skip
+    end
+  end
+end
